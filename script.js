d0be04a29587dec17b3987d937bdbf157611853e
MaxitoMix <128189550+MaxitoMix@users.noreply.github.com>
2023-05-16 12:34:19 -0400

    structure

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ function Gameboard() {
       board[i] = [];
       for (let j = 0; j < columns; j++) {
         board[i].push(Cell());
-
       }
     }
 
@@ -18,12 +17,74 @@ function Gameboard() {
 
 function Cell() {
     let value = 0;
+
+    const addToken = (player) => {
+      value = player;
+    };
     const getValue = () => value;
   
     return {
-        getValue
+        getValue, addToken
     };
-  }
+}
 
-const gameboard = Gameboard();
-console.log(gameboard.getBoard());
+function GameController(
+  playerOneName = "Player One",
+  playerTwoName = "Player Two"
+  ){
+    const board = Gameboard();
+
+    const players = [
+      {
+        name: playerOneName,
+        token: 'X'
+      },
+      {
+        name: playerTwoName,
+        token: 'O'
+      }
+    ];
+
+    let activePlayer = players[0];
+
+    const switchPlayerTurn = () => {
+      activePlayer = activePlayer === players[0] ? players[1] : players [0];
+    };
+
+    const getActivePlayer = () => activePlayer;
+
+    return {
+      getActivePlayer, switchPlayerTurn, getBoard: board.getBoard
+  };
+
+}
+  
+function ScreenController(){
+  const game = GameController();
+  const playerTurnDiv = document.querySelector('.instruction');
+  const boardContainer = document.querySelector('.board');
+
+  const updateScreen = () => {
+    boardContainer.textContent = "";
+    const board = game.getBoard();
+    const activePlayer = game.getActivePlayer();
+    playerTurnDiv.textContent = `${activePlayer.name}'s turn...`
+
+    board.forEach((row, rowIndex) => {
+      row.forEach((cell, columnIndex) => {
+        const button = document.createElement('button');
+        button.classList.add('cell');
+        button.dataset.row = rowIndex;
+        button.dataset.column = columnIndex;
+        button.textContent = cell.getValue();
+        boardContainer.appendChild(button);
+      })  
+    })
+  }
+  updateScreen();
+}
+
+
+ScreenController();
+
+
